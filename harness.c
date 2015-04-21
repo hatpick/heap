1b021c89c31fee91f33d2cdcf67b1ba821bd4c51
Soroush <hatpick@gmail.com>
2015-04-21 09:30:17 -0700

    fixed typo

--- harness.c
+++ harness.c
@@ -12,14 +12,14 @@ int cmpfunc (const void * a, const void * b)
 
 int main(int argc, char *argv[]){  
     int i, v;    
-    /* unsigned int s = nondet_unsigned_int();
-    __CPROVER_assume(s < SIZE);
-    printf ("LOG: size=%d\n", s);*/    
-    int *hsort_arr = (int *) malloc(sizeof(int) * SIZE);
-    int *qsort_arr = (int *) malloc(sizeof(int) * SIZE);
+    unsigned int s = nondet_unsigned_int();
+    __CPROVER_assume(s <= SIZE);
+    printf ("LOG: size=%d\n", s);
+    int *hsort_arr = (int *) malloc(sizeof(int) * s);
+    int *qsort_arr = (int *) malloc(sizeof(int) * s);
 
     /* generate random numbers to be sorted */
-    for(i = 0; i < SIZE; i++) {
+    for(i = 0; i < s; i++) {
         v = nondet_int();
         hsort_arr[i] = v;
         qsort_arr[i] = v;
@@ -27,23 +27,23 @@ int main(int argc, char *argv[]){
     }    
 
     /* initiate min heap */
-    minHeap hp = initMinHeap(SIZE);
+    minHeap hp = initMinHeap(s);
     /* build min heap */
-    buildMinHeap(&hp, hsort_arr, SIZE);
+    buildMinHeap(&hp, hsort_arr, s);
 
     /* check heap properties */
-    int minElementValue = hp.elem[0]->data;
-    for(i = 0; i < SIZE; i++){
-        assert(minElementValue <= hp.elem[i]->data);
+    int minElementValue = hp.elem[0].data;
+    for(i = 0; i < s; i++){
+        assert(minElementValue <= hp.elem[i].data);
     }
 
     /* sort the array in place: HEAP SORT */
-    heap_sort(&hp, hsort_arr, SIZE);
+    heap_sort(&hp, hsort_arr, s);
     /* sort the array in place: QSORT */
-    qsort(qsort_arr, SIZE, sizeof(int), cmpfunc);
+    qsort(qsort_arr, s, sizeof(int), cmpfunc);
 
     /* compare sorts */
-    for(i = 0; i < SIZE ; i++) {        
+    for(i = 0; i < s ; i++) {        
         assert(hsort_arr[i] == qsort_arr[i]);
         printf("LOG: (HSORT, QSORT) %d, %d\n", hsort_arr[i], qsort_arr[i]);
     }        
